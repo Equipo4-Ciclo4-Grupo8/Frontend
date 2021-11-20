44a3589fb664018f4be89e6aab25e02a2fb493a4
santigordillo15 <santi1596@hotmail.com>
2021-11-19 22:46:03 -0500

    frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, {useState} from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 import Footer from './componentes/Footer';
 import Navegacion from './componentes/Navegacion';
 import Login from './componentes/Login';
+import Thecategorias from './componentes/Thecategorias';
+import ServiciosOfertados from './componentes/ServiciosOfertados';
+import { categorias } from './Data/DataServicios';
 
 function App() {
+
+  const [nombreCategoria, setnombreCategoria] = useState()
+
+  console.log(nombreCategoria)
+    const handleCategorias = (index)=>{
+      setnombreCategoria(categorias[index].categoria.nombre)
+      }
   return (
     <Router>
       <Navegacion/>
@@ -14,6 +25,19 @@ function App() {
       <br/>
       <br/>
       <Footer/>
+
+      <Route exact path="/">
+    <div className="seccion-categorias"> 
+    <Thecategorias onClick={(index)=>handleCategorias(index)}/>
+    </div>
+    </Route>
+    
+    <Route path= "/servicios">
+    <div className="seccion-servicios">  
+    
+    <ServiciosOfertados nombreCategoria={nombreCategoria}  />
+    </div>
+    </Route>
     
     </Router>
   );
---
+++ src/componentes/ServiciosOfertados.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ServiciosOfertados.css'
+import { servicios } from '../Data/DataServicios';
+// import Thecategorias from './Thecategorias';
+
+
+
+const ServiciosOfertados =  props => {
+
+  // const servicios= [
+  //   {servicio:{
+  //     foto: "https://placeimg.com/220/220/people",
+  //     nombre:"Santiago Gordillo",
+  //     profesion:"Profesor de francés",
+  //     precio: 60000 , // si aplica o no
+  //     calificacion: "8/10",
+  //     categoria: "Tutores académicos",
+  //     celular: 666666,
+  //     correo: "@hotmail.com",
+  //     disponibilidadTiempo:"",
+  //     ciudad: "",
+  //     añosExperiencia: "",
+  //     fotosExperiencia:""
+  //     }},
+
+  
+  const servicioAMostrar = servicios.filter((item)=>
+      item.servicio.categoria === props.nombreCategoria
+    );//filtro para solo mostrar segun categoria
+
+    const ciclarServicios = servicioAMostrar.map((item, index) => {
+      
+        return (
+ 
+          <div key={index} className="card-servicio">
+             <div className="foto-perfil">
+                 <img src={item.servicio.foto} alt="foto"/>
+                  
+             </div>
+      
+             <div className="card-descripcion">
+                 <h3>{item.servicio.nombre}</h3>
+                 <h2 className="profesion">{item.servicio.profesion} </h2>
+                 <h3> $|Hora: {item.servicio.precio} COP </h3>
+                 <p>Calificación : {item.servicio.calificacion}</p>
+             </div>
+  
+           </div>
+        )
+      
+      
+        });
+      
+        return(
+        
+          ciclarServicios
+      
+        )
+      };
+
+      
+ServiciosOfertados.propTypes={
+    //servicio: PropTypes.object.isRequired //nota: agregar el tema de proptypes.sharp, para entrar dentro del objeto y definir los tipos de variables
+};
+
+ServiciosOfertados.defaultProps={
+
+}
+      
+
+export default ServiciosOfertados
