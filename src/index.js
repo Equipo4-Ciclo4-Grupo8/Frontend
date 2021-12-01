93c4e77b4e8a03ee278931fd6207249b4b47e91b
santigordillo15 <santi1596@hotmail.com>
2021-12-01 18:42:09 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Axios from 'axios'
+Axios.defaults.baseURL ='http://localhost:4000'
 
 ReactDOM.render(
   <React.StrictMode>
